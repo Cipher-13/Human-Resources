09a62f60b7875f04e4f261b8a99b13a0d1802e92
Cipher-13 <aihab6087@gmail.com>
2025-11-18 14:11:06 +0200

    Employee-projects

--- src/app/Features/employee/module/projects/projects.component.ts
+++ src/app/Features/employee/module/projects/projects.component.ts
@@ -7,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
-  
+    // عدد الأعمدة الفاضية
+  emptyColumns = Array(12).fill(null);
+
+  // بيانات المشاريع
+  projects = [
+    {
+      name: 'رمح',
+      task: 'فرونت اند الموقع',
+      logo: 'assets/images/rumh-project.png',
+      department: 'البرمجة',
+      tasksCount: 5,
+      status: 'completed',
+      statusText: 'مكتمل'
+    },
+    {
+      name: 'افاق السبل',
+      task: 'واجهات الموقع',
+      logo: 'assets/images/afaq-Project.png',
+      department: 'البرمجة',
+      tasksCount: 3,
+      status: 'in-progress',
+      statusText: 'قيد التنفيذ'
+    },
+    {
+      name: 'عناية',
+      task: 'واجهات التطبيق',
+      logo: 'assets/images/einya-project.png',
+      department: 'البرمجة',
+      tasksCount: 3,
+      status: 'in-progress',
+      statusText: 'قيد التنفيذ'
+    }
+  ];
+
+
 }
