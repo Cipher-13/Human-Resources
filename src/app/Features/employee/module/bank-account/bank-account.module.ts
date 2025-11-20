f86bf879409a691621c362e81792724c8b12a25f
Cipher-13 <aihab6087@gmail.com>
2025-11-20 10:53:26 +0200

    add-modal phase1.1

--- src/app/Features/employee/module/bank-account/bank-account.module.ts
+++ src/app/Features/employee/module/bank-account/bank-account.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { BankAccountRoutingModule } from './bank-account-routing.module';
 import { BankAccountComponent } from './bank-account.component';
 import { SharableModule } from 'src/app/Shared/sharable/sharable.module';
-import { AddEditComponent } from './Components/add-edit/add-edit.component';
 
 
 @NgModule({
   declarations: [
     BankAccountComponent,
-    AddEditComponent,
    ],
   imports: [
     CommonModule,
