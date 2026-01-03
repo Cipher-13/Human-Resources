2b1fe435b5e6d3e7cb73357bb41684adf5244578
Cipher-13 <aihab6087@gmail.com>
2026-01-03 13:55:11 +0200

    forms

--- src/app/Features/forms/forms.component.ts
+++ src/app/Features/forms/forms.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+interface ContractRow {
+  name: string;
+  type: string;
+  creator: string;
+  avatarUrl: string;
+}
+
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+  contracts: ContractRow[] = [
+    {
+      name: 'شهادة خبره',
+      type: '(Experience letter)',
+      creator: 'خالد حسن علي',
+      avatarUrl: 'assets/images/Guset-Avatar.png'
+    },
+    {
+      name: 'شهادة خبره',
+      type: '(Experience letter)',
+      creator: 'خالد حسن علي',
+      avatarUrl: 'assets/images/Guset-Avatar.png'
+    },
+    {
+      name: 'شهادة خبره',
+      type: '(Experience letter)',
+      creator: 'خالد حسن علي',
+      avatarUrl: 'assets/images/Guset-Avatar.png'
+    }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onPreview(contract: ContractRow): void {
+    console.log('Previewing:', contract.name);
+  }
+
+  onUseTemplate(contract: ContractRow): void {
+    console.log('Using template for:', contract.name);
+  }
 }
